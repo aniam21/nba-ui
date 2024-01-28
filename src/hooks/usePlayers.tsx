fe59aa5f57814cae616c585c6a57ca304ff0d89f
aniamwolff21 <aniamwolff21@gmail.com>
2024-01-28 16:59:01 +0200

    perf: used react Query for faster cache

--- src/hooks/usePlayers.tsx
+++ src/hooks/usePlayers.tsx
@@ -1,35 +1,32 @@
 import PlayersModule from 'services/players.service';
-import { useSetRecoilState } from 'recoil';
-import { playersState } from 'atoms/player.atoms';
 import { toast } from 'react-toastify';
 import { useFetch } from './useFetch';
 
 import { PlayerResponse } from '../utils/types/player.interfaces';
 
 function usePlayers() {
-  const setPlayers = useSetRecoilState<PlayerResponse>(playersState);
-
   const { fetchAsync } = useFetch();
 
-  const fetchPlayers = async (search = '', page = 1) => {
+  const fetchPlayers = async (search = '', page = 1): Promise<PlayerResponse> => {
     try {
-    const recievedPlayers = await fetchAsync<PlayerResponse>(PlayersModule.getPlayers({ search, page }));
-    const playersMap = new Map(Object.entries(recievedPlayers.data));
-    const formattedPlayers = {
-      metadata: recievedPlayers.metadata,
-      data: playersMap
-    };
-    setPlayers(formattedPlayers);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (err: any) {
-    const message = err.response?.message ?? err.message;
-    toast.error(message, {
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-    });
+      const recievedPlayers = await fetchAsync<PlayerResponse>(PlayersModule.getPlayers({ search, page }));
+      const playersMap = new Map(Object.entries(recievedPlayers.data));
+      const formattedPlayers = {
+        metadata: recievedPlayers.metadata,
+        data: playersMap
+      };
+      return formattedPlayers;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      const message = err.response?.message ?? err.message;
+      toast.error(message, {
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true
+      });
+      throw new Error(message);
+    }
   };
-};
 
   return { fetchPlayers };
 }
