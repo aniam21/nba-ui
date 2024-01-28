14534af8ee8da8c7f8c3fc89745ac0e7160f6cfc
aniamwolff21 <aniamwolff21@gmail.com>
2024-01-28 16:59:01 +0200

    perf: implement react query for player search

--- src/components/PlayersList/index.tsx
+++ src/components/PlayersList/index.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react';
 
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 
-import { loaderState, pageState } from 'atoms/filter.atoms';
+import { queryState } from 'atoms/filter.atoms';
 import { playersState } from 'atoms/player.atoms';
 
 import PaginationFooter from 'components/common/PaginationFooter';
@@ -11,6 +10,7 @@ import List from 'components/common/List';
 import EmptyFooter from 'components/common/EmptyList';
 import usePlayers from 'hooks/usePlayers';
 
+import { useQuery } from '@tanstack/react-query';
 import SpinningLoader from 'components/common/SpinningLoader';
 import ListItem from 'components/common/ListItem';
 import { PlayerResponse } from '$utils/types/player.interfaces';
@@ -19,41 +19,52 @@ import styles from './index.module.css';
 
 const AllPlayerList = () => {
   const { fetchPlayers } = usePlayers();
-  const loadingState = useRecoilValue(loaderState);
 
-  const players = useRecoilValue<PlayerResponse>(playersState);
-  const [page, setPage] = useRecoilState(pageState);
-  const [search, setSearch] = useState("");
+  const [players, setPlayers] = useRecoilState<PlayerResponse>(playersState);
+  const [query, setQuery] = useRecoilState(queryState);
 
-  const { metadata, data } = players;
+  const handleSuccess = (data: PlayerResponse) => {
+    setPlayers(data);
+  }
+  const queryRes = useQuery<PlayerResponse>({
+    queryKey: ['playersData', query.page, query.search],
+    queryFn: () => fetchPlayers(query.search, query.page),
+    refetchOnWindowFocus: false
+  });
 
-  useEffect(() => {
-    fetchPlayers(search, page);
-  }, [page, search]);
+  useQuery({
+    queryKey: ['playersData', query.page+1, query.search],
+    queryFn: () => fetchPlayers(query.search, query.page+1),
+    enabled: queryRes.isFetched && queryRes.isSuccess && !!players.metadata.nextPage,
+  });
 
   const handleSearch = (searchTerm: string) => {
-    if (searchTerm === '' || page !== 1 || search !== searchTerm) {
-      setPage(1);
+    let newPage = query.page;
+    if (searchTerm === '' || query.page !== 1 || query.search !== searchTerm) {
+      newPage = 1;
     }
-    setSearch(searchTerm);
+    setQuery({ page: newPage, search: searchTerm });
   };
 
-  const noPlayersFound = !data.size && !loadingState;
+  const noPlayersFound = queryRes.data && !queryRes.data.data.size && !queryRes.isLoading;
+
+  if (queryRes.isError) return <div>Something went wrong ...</div>;
+  if (queryRes.isSuccess) handleSuccess(queryRes.data);
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.loader}>
-        <SpinningLoader visible={loadingState} />
+        <SpinningLoader visible={queryRes.isLoading} />
       </div>
-      <div className={loadingState ? styles.loading : ''}>
+      <div className={queryRes.isLoading ? styles.loading : ''}>
         <List
           label="All NBA Players"
           footer={
             <PaginationFooter
               metadata={{
-                totalPages: metadata.totalPages || 1,
-                nextPage: metadata.nextPage || 1,
-                currentPage: page
+                totalPages: players.metadata.totalPages || 1,
+                nextPage: players.metadata.nextPage || 1,
+                currentPage: query.page
               }}
             />
           }
@@ -64,7 +75,7 @@ const AllPlayerList = () => {
               <EmptyFooter label="No Players Found" />
             </div>
           ) : (
-            Array.from(data.entries()).map(([key, player]) => (
+            Array.from(players.data.entries()).map(([key, player]) => (
               <div key={key} className={styles.list__container}>
                 <ListItem data={player} />
               </div>
